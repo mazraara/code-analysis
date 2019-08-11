856727ce9d9ed9f71b990eba77a69b736a0eb64c
Azraar Azward <mazraara@gmail.com>
2019-08-11 08:27:59 +0530

    using new package mazraara/random-password

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use RandomPassword\Password;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $password = new Password(10);
+    return $password->generate();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
